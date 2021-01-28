7736bcffd9d98fd85f7aa6a965ddd94158a14c10
thanakorn <thanakorn.panyapiang@outlook.com>
2021-01-28 22:02:10 +0700

    Update

--- scrape_land_price_data.py
+++ scrape_land_price_data.py
@@ -37,28 +37,33 @@ def get_land_data(land_no, survey_no, province_no):
     # Extract Land Id
     land_request_data = {
                             'chanode_no': land_no,
-                            'survey_no': survey_no,
+                            # 'survey_no': survey_no,
                             'selChangwat': province_no
                         }
     land_request_body = parse.urlencode(land_request_data).encode()
     land_request = request.Request(land_url, land_request_body)
     land_response = request.urlopen(land_request)
-    landprice_params = re.search('LandReport\(\d+,\d+,\S+\)', str(land_response.read())).group(0)
-    params = re.search('\(\S+\)', landprice_params).group(0)[1:-2]
-    params = [int(re.search('\d+', id).group(0)) for id in params.split(',')]
-    land_id, province_id, amphur_id = params[0], params[1], params[2]
-    
-    # Scrape Land data
-    landprice_response = request.urlopen(price_url % (land_id, province_id, amphur_id))
-    raw_data = landprice_response.read()
-    data = raw_data.decode('TIS-620') # Thai Language Characters Set
-    parser = DataParser()
-    parser.feed(data)
-    landprice_info = parser.output[1:].split(';')
-    if len(landprice_info) > 14:
-        unwanted_cols = len(landprice_info) - 14
-        landprice_info = landprice_info[0:12] + landprice_info[12 + unwanted_cols:]
-    return landprice_info
+    land_response = str(land_response.read())
+    landprice_params = re.findall('LandReport\(\d+,\d+,\S+\)', land_response)
+    results = []
+    for p in landprice_params:
+        params = re.search('\(\S+\)', p).group(0)[1:-2]
+        params = [int(re.search('\d+', id).group(0)) for id in params.split(',')]
+        land_id, province_id, amphur_id = params[0], params[1], params[2]
+        
+        # Scrape Land data
+        landprice_response = request.urlopen(price_url % (land_id, province_id, amphur_id))
+        raw_data = landprice_response.read()
+        data = raw_data.decode('TIS-620') # Thai Language Characters Set
+        parser = DataParser()
+        parser.feed(data)
+        landprice_info = parser.output[1:].split(';')
+        if len(landprice_info) > 14:
+            unwanted_cols = len(landprice_info) - 14
+            landprice_info = landprice_info[0:12] + landprice_info[12 + unwanted_cols:]
+        results.append(landprice_info)
+        
+    return results
     
     
 
@@ -66,28 +71,32 @@ def get_ns3_data(ns3a_no, rawang_no, province_no):
     # Extract NS3A Id
     ns3a_request_data = {
                             'ns3a_no': ns3a_no,
-                            'rawang_landno': rawang_no,
+                            # 'rawang_landno': rawang_no,
                             'selChangwat': province_no
                         }
     ns3a_request_body = parse.urlencode(ns3a_request_data).encode()
     ns3a_request = request.Request(ns3_url, ns3a_request_body)
     ns3a_response = request.urlopen(ns3a_request)
-    ns3a_params = re.search('NS3AReport\(\d+,\S+\)', str(ns3a_response.read())).group(0)
-    params = re.search('\(\S+\)', ns3a_params).group(0)[1:-2]
-    params = [int(re.search('\d+', id).group(0)) for id in params.split(',')]
-    ns3aid = params[0]
-    
-    # Scrape NS3A data
-    ns3a_response = request.urlopen(ns3data_url % ns3aid)
-    raw_data = ns3a_response.read()
-    data = raw_data.decode('TIS-620')
-    parser = DataParser()
-    parser.feed(data)
-    ns3a_info = parser.output[1:].split(';')
-    if len(ns3a_info) > 13:
-        unwanted_cols = len(ns3a_info) - 13
-        ns3a_info = ns3a_info[0:12] + ns3a_info[12 + unwanted_cols:]
-    return ns3a_info
+    ns3a_response = str(ns3a_response.read())
+    ns3a_params = re.findall('NS3AReport\(\d+,\S+\)', ns3a_response)
+    results = []
+    for params in ns3a_params:
+        params = re.search('\(\S+\)', params).group(0)[1:-2]
+        params = [int(re.search('\d+', id).group(0)) for id in params.split(',')]
+        ns3aid = params[0]
+        
+        # Scrape NS3A data
+        ns3a_response = request.urlopen(ns3data_url % ns3aid)
+        raw_data = ns3a_response.read()
+        data = raw_data.decode('TIS-620')
+        parser = DataParser()
+        parser.feed(data)
+        ns3a_info = parser.output[1:].split(';')
+        if len(ns3a_info) > 13:
+            unwanted_cols = len(ns3a_info) - 13
+            ns3a_info = ns3a_info[0:12] + ns3a_info[12 + unwanted_cols:]
+        results.append(ns3a_info)
+    return results
 
 def is_excluded(data):
     for ex in excluded:
@@ -116,8 +125,9 @@ for file in input_files:
         try:
             land_no, survey_no, province_no = deeds.loc[i, 'Title Deed No..1'], int(deeds.loc[i, 'หน้าสำรวจ']), deeds.loc[i, 'Code']
             land_data = get_land_data(land_no, survey_no, province_no)
-            land_output.write(';'.join(land_data))
-            land_output.write('\n')
+            for data in land_data:
+                land_output.write(';'.join(data))
+                land_output.write('\n')
             print(f'Saved.')
         except:
             print(f'No data')
@@ -130,8 +140,9 @@ for file in input_files:
         try:
             ns3a_no, rawang_no, province_no = ns3as.loc[i, 'Title Deed No..1'], int(ns3as.loc[i, 'เลขที่ดิน']), ns3as.loc[i, 'Code']
             ns3a_data = get_ns3_data(ns3a_no, rawang_no, province_no)
-            ns3a_output.write(';'.join(ns3a_data))
-            ns3a_output.write('\n')
+            for data in ns3a_data:
+                ns3a_output.write(';'.join(data))
+                ns3a_output.write('\n')
             print('Saved')
         except:
             print('No data')
